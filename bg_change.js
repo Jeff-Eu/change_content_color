b36456954e7f2132c93bde1147cbd1ca8f7ddb9c
euleramon <euleramon@gmail.com>
2017-01-10 15:54:18 +0800

    More areas can be changed.
    Now, more area background color can be changed.

--- bg_change.js
+++ bg_change.js
@@ -1,6 +1,9 @@
-              
-      $(document).dblclick(function (e) {      
-        $(e.target).css('background-color', '#CECCB5');        
-      });
-      
-chrome.runtime.sendMessage({action: "showIcon"}, function(response) {});
+// Used method: http://stackoverflow.com/a/11963008/1613961
+// Another cool method but not used: http://stackoverflow.com/a/8894528/1613961
+$(document).dblclick(function (e) {
+  $(e.target).each(function() {
+    this.style.setProperty('background-color', '#CECCB5','important');
+  })
+});
+
+chrome.runtime.sendMessage({ action: "showIcon" }, function (response) { });
